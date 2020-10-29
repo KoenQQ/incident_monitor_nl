ff7c7f20d9e0bd7b860e79437a1e9ffbea214b67
KoenQQ <koen@cartha.io>
2020-10-29 10:47:09 +0100

    where is this going?

--- scraper/management/commands/scrape.py
+++ scraper/management/commands/scrape.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 import json
 from django.core.management.base import BaseCommand, CommandError
-
+import pandas as pd
 import requests as rq
+import cProfile
 
 from bs4 import BeautifulSoup
 
@@ -25,24 +26,50 @@ class Command(BaseCommand):
 
         #convert to soup
         soup = BeautifulSoup(xml_data,"html.parser")
-
+        p
         # grab all incidents, met toebehoren
         incident_messages = soup.find_all("item")
 
+        # #df that can take all items
+        # incident_columns = ['title', 'description' 'geo_lat', 'geo_long', 'pubDate']
+        incidents_data = pd.DataFrame() 
+
+
         for i in incident_messages:
+            call = []
             title = i.find('title').text
             description = i.find('description').text
-            geo_lat = i.find('geo:lat') # uit find() komt NoneType of het element
+            geo_lat = i.find('geo:lat')
             geo_long = i.find('geo:long')
-            pubDate = i.find('pubdate').text
+            pubDate = i.find('pubdate').text 
+            
+            call.append([title, description, geo_lat, geo_long, pubDate])
+            incidents_data = incidents_data.append(call)
+        
+        
+        incidents_data['emergency code'] = incidents_data['description'].regex
+        print(incidents_data['emergency code'])
+
+        # split the 'description' column up in multiple columns
+        for r in incidents_data:
+            incidents_data['code'] = incidents_data['description'].apply(lambda x: re.search(r'\d+', x).group())
+            incidents_data['e_service'] = incidents_data['description'].apply(lambda x: re.search(r'', x).group())
+            #finetune and add
+
+        incidents_data.rename(columns={"0": "title", "1": "description", "2" : "geo_lat", "3" : "geo_long", "4" : "pubDate" })
+        # print(incidents_data.head())
 
             # check if url in db
+        for s in incidents_data:
             try:
+                # dit moet save to dataframe worden
                 # save in db
-                # must match model
+                # must match Model
                 Incidents.objects.create(
                     title=title,
                     description = description if description is not None else " ",
+                    message_code = 
+                    emergency_service = 
                     geo_lat = geo_lat.text if geo_lat is not None else " ",
                     geo_long = geo_long.text if geo_long is not None else " ",
                     pubDate = pubDate
@@ -50,5 +77,5 @@ class Command(BaseCommand):
                 print('%s added' % (title,))
             except:
                 print('%s could not add' % (title, ))
-
+        
         self.stdout.write( 'job complete' )
--- scraper/models.py
+++ scraper/models.py
@@ -15,11 +15,21 @@ from django.db import models
 class Incidents(models.Model):
     """Determines entry fields for scraper """
 
+    #hulpdienst opties
+    e_service_choices = (
+        ('br', 'brandweer'),
+        ('pol', 'politie'),
+        ('ambu', 'ambulance')
+    )
+
+
     title = models.CharField(max_length=250, primary_key=True, unique=False)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250) #full description 
+    message_code = models.CharField(max_length=100) #hier ook optie lijst maken? 
+    emercency_service = models.CharField(max_length=100, choices = e_service_choices)
     geo_lat = models.CharField(max_length=250, blank=True, null=True)  
     geo_long = models.CharField(max_length=250, blank=True, null=True)
-    pubDate = models.CharField(max_length=250)
+    pubDate = models.CharField(max_length=250) 
 
     def __str__(self):
         return self.title
