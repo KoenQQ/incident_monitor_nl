d807f44ee09c75e485c29db0919b19aa6029eced
Koen de Brauw <66774794+KoenQQ@users.noreply.github.com>
2021-02-22 12:34:27 +0100

    Update views.py

--- scraper/views.py
+++ scraper/views.py
@@ -91,7 +91,7 @@ class NearbyIncidents(viewsets.ModelViewSet):
         # searchRange = return_degrees(searchRange)
         print(searchRange)
          # point = Point(lng, lat) #gebruik voor hardcode locatie       
-        queryset = Incidents.objects.filter(pub_date__gte=datetime.now()-timedelta(days=dateRange)).filter(location__dwithin=(point, 0.009))
+        queryset = Incidents.objects.filter(pub_date__gte=datetime.now()-timedelta(days=dateRange)).filter(location__dwithin=(point, searchRanged))
         recent_incidents_list = queryset
         updated_incidents_list = serializers.serialize("json", recent_incidents_list)
         return HttpResponse(updated_incidents_list)
