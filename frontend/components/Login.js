c69de8668aeee31750b1d54b7ed9235d06631962
Koen de Brauw <66774794+KoenQQ@users.noreply.github.com>
2021-04-08 13:49:50 +0200

    update tekst

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -45,9 +45,9 @@ class Login extends React.Component {
                     De Incidentradar helpt sneller en adequater te reageren op incidenten om zo negatieve effecten te verminderen en preventie te verbeteren.  </div>
                     <br /><br />
                     <div>Gebruik de Incidentradar om..  </div>
-                    <div>1. .. automatisch gewaarschuwd te worden als er iets bij je een van je adressen/in je buurt wat aan de hand is, waardoor je sneller kan reageren en eventuele schade kan beperken.</div>
-                    <div>2. .. eenvoudig de incidentendata voor eigen inzichten te gebruiken. </div>
-                    <div>3. .. (toekomst) locatie risico inzichten op basis van historische data te krijgen.</div>
+                    <div>1. .. automatisch gewaarschuwd te worden als er een incident plaatsvindt bij een van je gekozen locaties, waardoor je sneller kan reageren en eventuele schade kan beperken.</div>
+                    <div>2. .. de eigen risicoanalyse te verdiepen met extra informatie. </div>
+                    <div>3. .. locatie gerelateerde risico inzichten op basis van historische data te krijgen. (toekomst)</div>
                     <br /><br /><br /><br />
                     <div>De Incidentradar is op dit moment in Private Beta. Geïnteresseerd in een demo? Stuur een bericht naar koen@cartha.io. </div>
                </div> 
