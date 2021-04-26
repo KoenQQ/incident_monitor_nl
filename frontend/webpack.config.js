371be7d86e4ac2e13c6434d82fbe485b2ffb607c
Koen de Brauw <66774794+KoenQQ@users.noreply.github.com>
2021-04-26 13:52:02 +0200

    image & NPM env update

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         process: 'process/browser',
       }),
       new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.NODE_ENV': JSON.stringify("development"),
         'process.env.NODE_DEBUG': JSON.stringify(process.env.NODE_DEBUG),
         'process.type': JSON.stringify(process.type),
         'process.version': JSON.stringify(process.version),
