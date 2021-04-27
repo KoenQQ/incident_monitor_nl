a0811aea8771a034db6d74e84547443c5ef7a71e
Koen de Brauw <66774794+KoenQQ@users.noreply.github.com>
2021-04-27 09:20:36 +0200

    Update urls.py

--- incident_webscraper/urls.py
+++ incident_webscraper/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from scraper import views
 from rest_framework_jwt.views import obtain_jwt_token
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('frontend.urls')), 
@@ -11,7 +11,5 @@ urlpatterns = [
     path('scraper/', include('scraper.urls')),
     path('token-auth/', obtain_jwt_token),
     path('accounts/', include('django.contrib.auth.urls'))
-] 
-
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
